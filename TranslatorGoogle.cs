0e940f1a3cea8c1b403cc4606f5129829e70031d
Eduardo <eduardo.xbox@live.com>
2023-09-20 23:48:32 -0300

    replaces WebClient with HttpClient and makes the TranslateString method asynchronous to work with HttpClient's async methods

--- TranslatorGoogle.cs
+++ TranslatorGoogle.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,42 +8,53 @@ using Newtonsoft.Json.Linq;
 
 namespace VPet.Plugin.AutoMTL
 {
-	class TranslatorGoogle : TranslatorBase
-	{
-		public static new string providerName = "Google Translate";
-		public static new string providerId =  "google";
-		public static new Dictionary<string, string> providedLanguages => JsonConvert.DeserializeObject<Dictionary<string, string>>(Constants.googleLangJSON);
+    class TranslatorGoogle : TranslatorBase
+    {
+        public static new string providerName = "Google Translate";
+        public static new string providerId = "google";
+        public static new Dictionary<string, string> providedLanguages => JsonConvert.DeserializeObject<Dictionary<string, string>>(Constants.googleLangJSON);
 
-		public TranslatorGoogle(string srcLang, string dstLang, string cacheBase) : base(srcLang,dstLang,cacheBase)
-		{
-		}
+        public TranslatorGoogle(string srcLang, string dstLang, string cacheBase) : base(srcLang, dstLang, cacheBase)
+        {
+        }
 
-		// https://stackoverflow.com/questions/50963296/c-sharp-google-translate-without-api-and-with-unicode
-		// https://github.com/dmytrovoytko/SublimeText-Translate/blob/master/Translator.py
-		public override string TranslateString(string input)
-		{
-			try {
-				string url = String.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}", srcLang, dstLang, input);
-				
-				WebClient webClient = new WebClient();
-				webClient.Encoding = System.Text.Encoding.UTF8;
-				string result = webClient.DownloadString(url);
-				JArray json = (JArray)JsonConvert.DeserializeObject(result);
+        // https://stackoverflow.com/questions/50963296/c-sharp-google-translate-without-api-and-with-unicode
+        // https://github.com/dmytrovoytko/SublimeText-Translate/blob/master/Translator.py
+        public override async Task<string> TranslateStringAsync(string input)
+        {
+            try
+            {
+                string url = $"https://translate.googleapis.com/translate_a/single?client=gtx&sl={srcLang}&tl={dstLang}&dt=t&q={Uri.EscapeDataString(input)}";
 
-				string output = "";
-				foreach (JArray item in json[0])
-					output += item[0].Value<string>();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+                    
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        JArray json = (JArray)JsonConvert.DeserializeObject(result);
 
-				if (output == "")
-					return null;
+                        string output = "";
+                        foreach (JArray item in json[0])
+                            output += item[0].Value<string>();
 
-				return output;
-			} catch (Exception exc) {
-				string error = exc.ToString();
-				//MessageBox.Show(error);
-				return null;
-			}
+                        if (output == "")
+                            return null;
 
-		}
-	}
+                        return output;
+                    }
+                }
+
+                return null;
+            }
+            catch (Exception exc)
+            {
+                string error = exc.ToString();
+                // Handle the exception as needed.
+                return null;
+            }
+        }
+    }
 }
